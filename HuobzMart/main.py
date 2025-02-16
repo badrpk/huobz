54a15bbae816c56fc9cd4d783cc3da5f3ea2f858
badrpk <badrpk@gmail.com>
2025-02-16 06:39:01 +0000

    🚀 Added HuobzMart module

---
+++ HuobzMart/main.py
@@ -0,0 +1,9 @@
+import time
+
+def start_huobzmart():
+    print("✅ HuobzMart is running...")
+    while True:
+        time.sleep(10)  # Keeps it alive
+
+if __name__ == "__main__":
+    start_huobzmart()
